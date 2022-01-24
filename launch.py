811e51255ba9a7d6571bbb3d8a53a43de449781b
sjakedude <sjakedude@gmail.com>
2022-01-23 19:08:24 -0500

    bugfix

--- launch.py
+++ launch.py
@@ -2,24 +2,27 @@ import os
 from lib.date import Date
 from lib.task import Task
 
+
 def display_tasks(date):
     index = 1
     for task in date.tasks:
         print(str(index) + ". " + task.name)
 
-def main():
-    print("Here we go...")
-    date = input("Enter date: (MM/DD/YYYY) ")
-    date = Date(date)
-    display_tasks(date)
-    answer = input("Add task? (Y or N) ")
-    if answer == "Y":
-        task = input("Enter task: ")
-        task = Task(task)
-        date.add_task(task)
-    display_tasks(date)
 
-    
-    
+def main():
+    while True:
+        input_date = input("Enter date: (MM/DD/YYYY) ")
+        print("u entered")
+        print(input_date)
+        date = Date(input_date)
+        display_tasks(date)
+        answer = input("Add task? (Y or N) ")
+        if answer == "Y":
+            input_task = input("Enter task: ")
+            task = Task(input_task)
+            date.add_task(task)
+        display_tasks(date)
+        print()
+
 
 main()
